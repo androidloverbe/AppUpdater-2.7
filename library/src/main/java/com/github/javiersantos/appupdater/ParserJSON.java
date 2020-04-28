ff82978446ba414800e2850dd8cafad9d023f228
rscalover <rscalover@gmail.com>
2020-04-28 13:33:23 +0200

    updated

--- library/src/main/java/com/github/javiersantos/appupdater/ParserJSON.java
+++ library/src/main/java/com/github/javiersantos/appupdater/ParserJSON.java
@@ -15,7 +15,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 class ParserJSON {
     private URL jsonUrl;
@@ -74,13 +74,10 @@ class ParserJSON {
     }
 
     private JSONObject readJsonFromUrl() throws IOException, JSONException {
-        InputStream is = this.jsonUrl.openStream();
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+        try (InputStream is = this.jsonUrl.openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
             String jsonText = readAll(rd);
             return new JSONObject(jsonText);
-        } finally {
-            is.close();
         }
     }
 
